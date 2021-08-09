ea18a5de7cebc56900a5097098cded8a1b4ae339
allenalvin333 <allenalvin333@gmail.com>
2021-08-09 05:38:53 +0530

    programs

---
+++ SQL/13.sql
@@ -0,0 +1,6 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+SELECT CITY,LENGTH(CITY) FROM STATION
+ORDER BY LENGTH(CITY), CITY ASC LIMIT 1;
+SELECT CITY,LENGTH(CITY) FROM STATION
+ORDER BY LENGTH(CITY) DESC LIMIT 1;

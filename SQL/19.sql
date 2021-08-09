5d8dadba53c5cda09650a00e50ceaacf65193301
allenalvin333 <allenalvin333@gmail.com>
2021-08-09 06:08:00 +0530

    programs

--- SQL/19.sql
+++ SQL/19.sql
@@ -2,4 +2,4 @@
 
 SELECT DISTINCT CITY FROM STATION
 WHERE LEFT(CITY,1) NOT IN ('a','e','i','o','u')
-AND RIGHT(CITY,1) NOT IN ('a','e','i','o','u');
+OR RIGHT(CITY,1) NOT IN ('a','e','i','o','u');
---
+++ SQL/20.sql
@@ -0,0 +1,2 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-12/problem
+

e911c930a2d269b060ca66bdf3d71391655e3832
allenalvin333 <allenalvin333@gmail.com>
2021-08-11 06:45:08 +0530

    programs

--- SQL/35.sql
+++ SQL/35.sql
@@ -1,6 +1,6 @@
 -- https://www.hackerrank.com/challenges/weather-observation-station-15/problem
 
-Select ROUND(LONG_W,4) FROM STATION
+SELECT ROUND(LONG_W,4) FROM STATION
 WHERE LAT_N = 
 (SELECT MAX(LAT_N) FROM STATION
 WHERE LAT_N<137.2345)

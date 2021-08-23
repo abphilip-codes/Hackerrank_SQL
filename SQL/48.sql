ade4b99ba75654f536d340eebc018fa2fc4bf24c
allenalvin333 <allenalvin333@gmail.com>
2021-08-23 19:46:18 +0530

    programs

---
+++ SQL/48.sql
@@ -0,0 +1,6 @@
+-- https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
+
+SELECT COUNTRY.CONTINENT,FLOOR(AVG(CITY.POPULATION))
+FROM CITY INNER JOIN COUNTRY
+WHERE COUNTRY.CODE=CITY.COUNTRYCODE
+GROUP BY COUNTRY.CONTINENT

8977628ef6155ceb893b0db3f9c1694d1601c87e
allenalvin333 <allenalvin333@gmail.com>
2021-08-30 12:22:35 +0530

    programs

--- SQL/56.sql
+++ SQL/56.sql
@@ -1,2 +1,28 @@
 -- https://www.hackerrank.com/challenges/print-prime-numbers/problem
 -- MS SQL
+
+
+DECLARE @I INT=2;
+DECLARE @P INT = 0;
+DECLARE @ANS NVARCHAR(1000) = ''; 
+
+WHILE (@I<=1000)
+BEGIN
+   DECLARE @J INT = @I-1;
+   SET @P = 1;
+   WHILE(@J > 1)
+   BEGIN
+      IF @I % @J = 0
+      BEGIN 
+         SET @P = 0;
+      END
+    SET @J = @J - 1;
+   END
+   IF @P = 1
+   BEGIN
+      SET @ANS += CAST(@I AS NVARCHAR(1000)) + '&';
+   END
+SET @I = @I + 1;
+END
+SET @ANS = SUBSTRING(@ANS, 1, LEN(@ANS) - 1)
+SELECT @ANS
